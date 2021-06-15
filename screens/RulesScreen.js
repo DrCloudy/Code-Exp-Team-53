41e85edb6a00e5e494a537907fa3cd56861fe74f
Junk4i <kohjunkai31@gmail.com>
2021-06-15 15:46:59 +0800

    Downloaded country flag pack, added header and button. Cannot fix black icon

--- screens/RulesScreen.js
+++ screens/RulesScreen.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Appbar } from 'react-native-paper';
+import { IconButton, Colors } from 'react-native-paper';
+
+
 
 export default function RulesScreen() {
+  // const _goBack = () => console.log('Went back');
+
+  // const _handleSearch = () => console.log('Searching');
+
+  // const _handleMore = () => console.log('Shown more');
+  var country;
+  if(true){
+    country = require('../country-flags (Icon made by Freepik from www.flaticon.com)/png/011-china.png');
+  }
+  // {{uri: 'https://img.icons8.com/ios/452/react-native--v1.png'}}
   return (
-    <View style={styles.container}>
-      <Text>Rules and Regulations</Text>
-    </View>
+      <Appbar.Header style={{backgroundColor: "tomato"}}>
+        {/* <Appbar.BackAction onPress={_goBack} /> */}
+        {/* <Appbar.Action icon="magnify" onPress={_handleSearch} /> */}
+        {/* <Appbar.Action icon="dots-vertical" onPress={_handleMore} /> */}
+        <Appbar.Content title="Rules and Regulations" style = {styles.Headstyle} color = "white"/>
+        <IconButton
+        icon = {country}
+        size={30}
+        color = 'rgba(255, 0, 255, 0.0)'
+        onPress={() => console.log('Pressed')}/>
+    </Appbar.Header>
   );
 }
 
@@ -16,4 +38,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "rgba(255,0,0,0.5)",
   },
+  Headstyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
