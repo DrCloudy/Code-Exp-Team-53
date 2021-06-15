b778e3ec336a005163aa1c9a123fbf8fb5fdd93e
DrCloudy <rmriaz444@gmail.com>
2021-06-15 19:40:45 +0800

    location prompt shows after country selection

--- screens/SetupScreen.js
+++ screens/SetupScreen.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import { Text, View, StyleSheet, TouchableOpacity, Button } from "react-native";
 import { List } from "react-native-paper";
-import CountryList from "../components/CountryList.js";
-import LocationServices from "../components/LocationServices.js";
 
+/* Previous code - could not figure out how to pass var around components
 export default function SetupScreen({ navigation }) {
     return (
         <View style={styles.container}>
@@ -13,15 +12,74 @@ export default function SetupScreen({ navigation }) {
         </View>
     )
 }
+*/
+
+export default function test({ navigation }){
+    const [expanded, setExpanded] = React.useState(true);
+    const handlePress = () => setExpanded(!expanded);
+    const [select, setSelect] = useState(false);
+    
+    if (select){
+        return (
+          <View style={styles.locationContainer}>
+            <Text style={styles.text}>
+              For a better experience, please turn on device location, which
+              uses Google's location services.
+            </Text>
+            <View style={{ flexDirection: "row" }}>
+              <Button title="No Thanks" onPress={() => {}} />
+              <Button title="Okay" onPress={() => {}} />
+            </View>
+          </View>
+        );
+    } else {
+        return (
+          <View>
+            <List.Section style={styles.accordianContainer}>
+              <List.Accordion
+                title="Select Your Country"
+                left={(props) => <List.Icon {...props} icon="earth" />}
+                expanded={expanded}
+                onPress={handlePress}
+              >
+                <TouchableOpacity
+                  style={styles.listItem}
+                  onPress={() => {
+                    setSelect(true);
+                  }}
+                >
+                  <List.Item title="Singapore" />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.listItem}>
+                  <List.Item title="Malaysia" />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.listItem}>
+                  <List.Item title="Japan" />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.listItem}>
+                  <List.Item title="Thailand" />
+                </TouchableOpacity>
+              </List.Accordion>
+            </List.Section>
+          </View>
+        );
+    }
+        
+}
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#eee",
+    locationContainer: {
+        alignItems: "center",
+        height: "100%",
+        justifyContent: 'center',
+        backgroundColor: 'white',
     },
-    text: {
-        paddingTop: 60,
+        text: {
+        width: "100%",
         paddingLeft: 10,
-        margin: 5,
-        fontSize: 24, 
+        margin: 15,
+    },
+    accordianContainer: {
+        paddingTop: 30,
     }
 });
