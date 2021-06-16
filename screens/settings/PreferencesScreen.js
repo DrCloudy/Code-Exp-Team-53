b177358f24a27d2bf31db2bdd56cdff911dfdd33
allyfern72 <79740655+allyfern72@users.noreply.github.com>
2021-06-16 16:30:40 +0800

    Edited PreferencesScreen.js

--- screens/settings/PreferencesScreen.js
+++ screens/settings/PreferencesScreen.js
@@ -1,10 +1,106 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
 
 export default function prefScreen() {
+  const [sel0, setSel0] = useState(true);
+  const [sel1, setSel1] = useState(true);
+  const [sel2, setSel2] = useState(true);
+  const [sel3, setSel3] = useState(true);
+  const [sel4, setSel4] = useState(true);
+
+  function flip0() {
+    setSel0(!sel0);
+  }
+
+  function flip1() {
+    setSel1(!sel1);
+  }
+
+  function flip2() {
+    setSel2(!sel2);
+  }
+
+  function flip3() {
+    setSel3(!sel3);
+  }
+
+  function flip4() {
+    setSel4(!sel4);
+  }
+
+  const notifOptions = [
+    { title: "Outdoor sports", selector: sel0, id: "0" },
+    { title: "Indoor sports", selector: sel1, id: "1" },
+    { title: "Arts & craft", selector: sel2, id: "2" },
+    { title: "Clubs and pubs", selector: sel3, id: "3" },
+    { title: "Dining", selector: sel4, id: "4" },
+  ];
+
+  function renderItem({ item }) {
+    return (
+      <TouchableOpacity
+        style={styles.listButton}
+        onPress={() => {
+          if (item.id === "0") flip0();
+          if (item.id === "1") flip1();
+          if (item.id === "2") flip2();
+          if (item.id === "3") flip3();
+          if (item.id === "4") flip4();
+        }}
+      >
+        <View style={styles.listButtonView}>
+          <Text style={{ flex: 1 }}>{item.title}</Text>
+          {item.selector ? (
+            <Feather name="check-circle" size={24} color="black" />
+          ) : (
+            <Feather name="circle" size={24} color="black" />
+          )}
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
   return (
-    <View>
-      <Text>Edit preferences here</Text>
+    <View style={styles.settingsStyle}>
+      <Text style={styles.dispText}>
+        What activity updates are you interested in?
+      </Text>
+      <FlatList
+        style={styles.flatlistStyle}
+        data={notifOptions}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  dispText: {
+    fontSize: 18,
+    padding: 10,
+    alignItems: "left",
+  },
+  listButton: {
+    backgroundColor: "lightgrey",
+    padding: 10,
+    margin: 5,
+    borderRadius: 13,
+    alignItems: "center",
+  },
+  settingsStyle: {
+    alignItems: "center",
+    margin: 5,
+  },
+  flatlistStyle: {
+    width: "90%",
+  },
+  listButtonView: { flexDirection: "row", width: "100%", flex: 1 },
+});
